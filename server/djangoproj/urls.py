0d00adb610c8bb2b3de26401110a5ba734c81412
Tandu Yanmick <tyan20320@gmail.com>
2025-06-03 08:29:09 -0400

    @implemented dynamic pages

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
     path('login/', TemplateView.as_view(template_name='base.html')),
     path('register/', TemplateView.as_view(template_name='base.html')),
     path('dealers/', TemplateView.as_view(template_name="base.html")),
+    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="base.html")),
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="base.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
